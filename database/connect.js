1754d7ccb4df33a57772bb1f296d665dd35f2377
Herve <herve@Herves-iMac.local>
2021-04-13 16:22:49 +0800

    first upload to git

---
+++ database/connect.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+require('dotenv').config()
+
+
+function connect() {
+    mongoose.connect(process.env.MONGODB_DSN || 'mongodb://localhost/workout', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+    });
+}
+
+module.exports= {
+    connect,
+}
---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const {connect} = require("./database/connect");
+require('dotenv').config()
+const PORT = process.env.PORT || 3000;
+
+connect();
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// morgon logs each action we take on server
+app.use(logger("dev"));
+
+// allow access to public folder when it runs on server(localhost)
+app.use(express.static("public"));
+
+// htmlroutes
+app.use(require("./routes/htmlRoutes"));
+
+// api routes
+app.use(require("./routes/apiRoutes.js"));
+
+
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    console.log("we are connected");
+});
+
+// routesapp.use(require("./routes/api-routes.js"));app.use(require("./routes/html-routes.js"));
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}! Visit http://localhost:${PORT}/ in your browser`);
+});
